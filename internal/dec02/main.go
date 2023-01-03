3c612de52e4575a7c6fd6990377b31d6f259471d
Dustin Hooten <dustinhooten@gmail.com>
2023-01-02 17:43:21 -0700

    Funcify December 2

--- internal/dec02/main.go
+++ internal/dec02/main.go
@@ -1,151 +1,153 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"github.com/hooten/advent-of-code-2022/pkg/math"
+	"github.com/hooten/advent-of-code-2022/pkg/util"
 	"log"
 	"os"
 	"strings"
 )
 
-var elfHandLegend = map[string]string{
-	"A": "Rock",
-	"B": "Paper",
-	"C": "Scissors",
-}
-
-var handPointsLegend = map[string]int{
-	"X": 1,
-	"Y": 2,
-	"Z": 3,
-}
-
-var myHandPointsLegend = map[string]int{
-	"Rock":     1,
-	"Paper":    2,
-	"Scissors": 3,
-}
-
-var gamePointsLegend = map[string]int{
-	"A X": 3, // Rock Rock = Draw
-	"A Y": 6, // Rock Paper = Win
-	"A Z": 0, // Rock Scissors = Loss
-	"B X": 0, // Paper Rock = Loss
-	"B Y": 3, // Paper Paper = Draw
-	"B Z": 6, // Paper Scissors = Win
-	"C X": 6, // Scissors Rock = Win
-	"C Y": 0, // Scissors Paper = Loss
-	"C Z": 3, // Scissors Scissors = Draw
-}
-
-var resultLegend = map[string]int{
-	"X": 0,
-	"Y": 3,
-	"Z": 6,
-}
-
 func main() {
-	// partOne()
-	partTwo()
+	fmt.Printf("Part 1: Score is %d.\n", strategyGuidePoints(true))
+	fmt.Printf("Part 2: Score is %d.\n", strategyGuidePoints(false))
 }
 
-func partTwo() {
-	file, err := os.Open("./internal/dec02/input.txt")
+func strategyGuidePoints(xyzAsHand bool) int64 {
+	const filename = "./internal/dec02/input.txt"
+	bytes, err := os.ReadFile(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer file.Close()
+	lines := util.Filter(func(line string) bool {
+		return line != ""
+	}, strings.Split(string(bytes), "\n"))
 
-	scanner := bufio.NewScanner(file)
+	games := util.Map(func(line string) Game {
+		return ParseGame(line, xyzAsHand)
+	}, lines)
 
-	var myPoints int
-	for scanner.Scan() {
-		game := scanner.Text()
-		plays := strings.Split(game, " ")
-		if len(plays) != 2 {
-			log.Fatalf("unexpected length %d", len(plays))
-		}
-		elfHand := elfHandLegend[plays[0]]
-		myPlay := plays[1]
-		gamePoints := resultLegend[myPlay]
-		myHand := getMyHand(elfHand, gamePoints)
-		handPoints := myHandPointsLegend[myHand]
-		myPoints += handPoints + gamePoints
-		fmt.Printf("{elf: %s, me: %s, game: %d + %d, total: %d}\n", elfHand, myHand, handPoints, gamePoints, myPoints)
-	}
+	pointsPerGame := util.Map(func(game Game) int64 {
+		return game.Play()
+	}, games)
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("my score is %d", myPoints)
+	return math.Sum(pointsPerGame...)
 }
 
-func getMyHand(elfHand string, gamePoints int) string {
-	switch elfHand {
-	case "Rock":
-		switch gamePoints {
-		case 0:
-			return "Scissors"
-		case 3:
-			return "Rock"
-		case 6:
-			return "Paper"
-		default:
-			log.Fatalf("bad elf hand %q", elfHand)
-		}
-	case "Paper":
-		switch gamePoints {
-		case 0:
-			return "Rock"
-		case 3:
-			return "Paper"
-		case 6:
-			return "Scissors"
-		default:
-			log.Fatalf("bad elf hand %q", elfHand)
-		}
-	case "Scissors":
-		switch gamePoints {
-		case 0:
-			return "Paper"
-		case 3:
-			return "Scissors"
-		case 6:
-			return "Rock"
-		default:
-			log.Fatalf("bad elf hand %q", elfHand)
-		}
-	}
-	log.Fatalf("bad elf hand %s", elfHand)
-	return ""
+type Hand int64
+
+const (
+	Rock Hand = iota
+	Paper
+	Scissors
+)
+
+var HandPointsLegend = map[Hand]int64{
+	Rock:     1,
+	Paper:    2,
+	Scissors: 3,
 }
 
-func partOne() {
-	file, err := os.Open("./internal/dec02/input.txt")
-	if err != nil {
-		log.Fatal(err)
+var HandLegend = map[string]Hand{
+	"A": Rock,
+	"B": Paper,
+	"C": Scissors,
+	"X": Rock,
+	"Y": Paper,
+	"Z": Scissors,
+}
+
+func ParseHand(s string) Hand {
+	hand, ok := HandLegend[s]
+	if !ok {
+		log.Fatalf("could not parse hand %q", s)
 	}
-	defer file.Close()
+	return hand
+}
 
-	scanner := bufio.NewScanner(file)
+type Game struct {
+	player1 Hand
+	player2 Hand
+}
 
-	var myPoints int
-	for scanner.Scan() {
-		game := scanner.Text()
-		plays := strings.Split(game, " ")
-		if len(plays) != 2 {
-			log.Fatalf("unexpected length %d", len(plays))
+func ParseGame(s string, xyzAsHand bool) Game {
+	hands := strings.Split(s, " ")
+	if len(hands) != 2 {
+		log.Fatalf("expected 2 hands, got %q", s)
+	}
+	player1Hand := ParseHand(hands[0])
+	if xyzAsHand {
+		return Game{
+			player1: player1Hand,
+			player2: ParseHand(hands[1]),
 		}
-		myPlay := plays[1]
-		handPoints := handPointsLegend[myPlay]
-		gamePoints := gamePointsLegend[game]
-		myPoints += gamePoints + handPoints
 	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+	outcome := ParseOutcome(hands[1])
+	hand := DetermineHand(player1Hand, outcome)
+	return Game{
+		player1: player1Hand,
+		player2: hand,
 	}
+}
 
-	fmt.Printf("my score is %d", myPoints)
+func DetermineHand(hand Hand, outcome Outcome) Hand {
+	switch outcome {
+	case Win:
+		return (hand + 1) % 3
+	case Draw:
+		return hand
+	case Loss:
+		return (hand + 2) % 3
+	}
+	log.Fatalf("could not determine hand from hand %d and outcome %d", hand, outcome)
+	return -1
+}
+
+var OutcomeLegend = map[string]Outcome{
+	"X": Loss,
+	"Y": Draw,
+	"Z": Win,
+}
+
+func ParseOutcome(s string) Outcome {
+	outcome, ok := OutcomeLegend[s]
+	if !ok {
+		log.Fatalf("could not parse outcome %q", s)
+	}
+	return outcome
+}
+
+func (g Game) Play() int64 {
+	outcome := g.play()
+	handPoints, ok := HandPointsLegend[g.player2]
+	if !ok {
+		log.Fatalf("could not determine hand points for %q", g.player2)
+	}
+	return int64(outcome) + handPoints
+}
+
+type Outcome int64
+
+const (
+	Win  Outcome = 6
+	Draw         = 3
+	Loss         = 0
+)
+
+var plays = map[int64]Outcome{
+	-2: Loss,
+	-1: Win,
+	0:  Draw,
+	1:  Loss,
+	2:  Win,
+}
+
+func (g Game) play() Outcome {
+	play := g.player1 - g.player2
+	outcome, ok := plays[int64(play)]
+	if !ok {
+		log.Fatalf("could not determine outcome from play %d", play)
+	}
+	return outcome
 }
