25825d07568345b780f43d055e4850daa0d61f83
Dustin Hooten <dustinhooten@gmail.com>
2023-01-13 21:07:27 -0700

    Funcify December 4

--- internal/dec04/main.go
+++ internal/dec04/main.go
@@ -6,94 +6,91 @@ import (
 	"os"
 	"strconv"
 	"strings"
+
+	"github.com/hooten/advent-of-code-2022/pkg/util"
 )
 
-func main() {
-	//partOne()
-	partTwo()
+type Range struct {
+	Start, End int64
 }
 
-func readFile() string {
-	bytes, err := os.ReadFile("./internal/dec04/input.txt")
+func ParseRange(s string) (Range, error) {
+	ends := strings.Split(s, "-")
+	if len(ends) != 2 {
+		return Range{}, fmt.Errorf("cannot parse range %q", ends)
+	}
+	start, err := strconv.ParseInt(ends[0], 10, 64)
+	if err != nil {
+		return Range{}, fmt.Errorf("cannot parse range start %q", ends[0])
+	}
+	end, err := strconv.ParseInt(ends[1], 10, 64)
+	if err != nil {
+		return Range{}, fmt.Errorf("cannot parse range end %q", ends[1])
+	}
+	return Range{
+		Start: start,
+		End:   end,
+	}, nil
+}
+
+type Pair struct {
+	First, Second Range
+}
+
+func ParsePair(s string) (Pair, error) {
+	ranges := strings.Split(s, ",")
+	if len(ranges) != 2 {
+		return Pair{}, fmt.Errorf("cannot parse pair %q", ranges)
+	}
+	first, err := ParseRange(ranges[0])
+	if err != nil {
+		return Pair{}, err
+	}
+	second, err := ParseRange(ranges[1])
+	if err != nil {
+		return Pair{}, err
+	}
+	return Pair{
+		First:  first,
+		Second: second,
+	}, nil
+}
+
+func (p Pair) Overlapping(strict bool) bool {
+	return overlapping(p.First, p.Second, strict) || overlapping(p.Second, p.First, strict)
+}
+
+func overlapping(a, b Range, strict bool) bool {
+	if strict {
+		return a.Start <= b.Start && a.End >= b.End
+	}
+	return b.Start <= a.End && a.End <= b.End
+}
+
+func main() {
+	fmt.Printf("Part 1: There are %d fully overlapping pairs.\n", overlappingPairs(true))
+	fmt.Printf("Part 2: There are %d partly overlapping pairs.\n", overlappingPairs(false))
+}
+
+func overlappingPairs(strict bool) int64 {
+	bytes, err := os.ReadFile("./input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	return string(bytes)
-}
+	unfilteredLines := strings.Split(string(bytes), "\n")
+	lines := util.Filter(func(line string) bool {
+		return line != ""
+	}, unfilteredLines)
+	pairs := util.Map(func(line string) Pair {
+		pair, err := ParsePair(line)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return pair
+	}, lines)
+	overlapping := util.Filter(func(pair Pair) bool {
+		return pair.Overlapping(strict)
+	}, pairs)
 
-func partTwo() {
-	input := readFile()
-	lines := strings.Split(input, "\n")
-	total := 0
-	for _, line := range lines {
-		if line == "" {
-			continue
-		}
-		fmt.Println(line)
-		assignments := strings.Split(line, ",")
-		if len(assignments) < 2 {
-			log.Fatal("bad length", len(assignments))
-		}
-		m := make(map[int]int)
-		for _, assignment := range assignments {
-			rangeStr := strings.Split(assignment, "-")
-			start, _ := strconv.Atoi(rangeStr[0])
-			end, _ := strconv.Atoi(rangeStr[1])
-			for i := start; i <= end; i++ {
-				m[i]++
-			}
-		}
-		for i, n := range m {
-			if n > 1 {
-				total++
-				fmt.Println("overlap at", i)
-				break
-			}
-		}
-
-	}
-	fmt.Println("total", total)
-}
-
-func partOne() {
-	input := readFile()
-	lines := strings.Split(input, "\n")
-	total := 0
-	for _, line := range lines {
-		if line == "" {
-			continue
-		}
-		assignments := strings.Split(line, ",")
-		if len(assignments) < 2 {
-			fmt.Printf("%q", line)
-			log.Fatal("bad length")
-		}
-		oneAssign := strings.Split(assignments[0], "-")
-		twoAssign := strings.Split(assignments[1], "-")
-		one0, _ := strconv.Atoi(oneAssign[0])
-		two0, _ := strconv.Atoi(twoAssign[0])
-		one1, _ := strconv.Atoi(oneAssign[1])
-		two1, _ := strconv.Atoi(twoAssign[1])
-		if one0 <= two0 && one1 >= two1 {
-			fmt.Printf("one: %q is contained in %q\n", twoAssign, oneAssign)
-			total++
-			continue
-		}
-		if two0 <= one0 && two1 >= one1 {
-			fmt.Printf("two: %q is contained in %q\n", oneAssign, twoAssign)
-			total++
-			continue
-		}
-		fmt.Printf("%q is NOT contained in %q or vice versa\n", oneAssign, twoAssign)
-	}
-	fmt.Println("total", total)
-}
-
-func toSet(s string) map[string]bool {
-	as := strings.Split(s, "")
-	m := map[string]bool{}
-	for _, a := range as {
-		m[a] = true
-	}
-	return m
+	return int64(len(overlapping))
 }
